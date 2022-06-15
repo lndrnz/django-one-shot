5e5256d0ed9b667d914983d3e44a4561309b21cc
Lander Nunez <landernunez@gmail.com>
2022-06-14 23:13:12 -0700

    feature 7

--- todos/urls.py
+++ todos/urls.py
@@ -1,4 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
 
-urlpatterns = []
+from todos.views import (
+    TodoListListView,
+)
+
+urlpatterns = [
+    path("", TodoListListView.as_view(), name="todo_list_list"),
+]
--- todos/views.py
+++ todos/views.py
@@ -1,3 +1,15 @@
+from typing import List
+from django.urls import reverse_lazy
+from django.views.generic.detail import DetailView
+from django.views.generic.edit import CreateView, DeleteView, UpdateView
+from django.views.generic.list import ListView
+
+from todos.models import TodoItem, TodoList
 from django.shortcuts import render
 
 # Create your views here.
+
+
+class TodoListListView(ListView):
+    model = TodoList
+    template_name = "todo_lists/list.html"
